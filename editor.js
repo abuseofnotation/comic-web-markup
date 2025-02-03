3c9a69e15fd2363e530c0d8a337720f170cc1f01
Boris Marinov <marinovboris@protonmail.com>
2025-02-03 21:18:32 +0200

    stuff

--- editor.js
+++ editor.js
@@ -3,5 +3,5 @@ import {renderComic} from './lib/index.js'
 document.getElementById('script').addEventListener('input', (e) => {
   const comic = document.getElementById('comic')
   const script = e.target.value.split('* * *')
-  comic.replaceChildren(...script.map((panel) => renderComic(panel, "comics/")))
+  comic.replaceChildren(...script.map((panel) => renderComic(panel, "generic-comics/")))
 })
--- plugin.js
+++ plugin.js
@@ -3,12 +3,14 @@ import {renderComic} from './lib/index.js'
 const panels = 
 Array.prototype.slice.call(document.getElementsByClassName("panel"))
 panels.forEach((panel) => {
+  const urlDir = panel.src.split('/').slice(0, -1).join('/')
   if (panel.height < 40) {
-    const svgPanel= renderComic(panel.alt, panel.src)
+    const svgPanel= renderComic(panel.alt, urlDir)
     panel.replaceWith(svgPanel)
   } else {
   panel.addEventListener('error', () => {
-    const svgPanel= renderComic(panel.alt, panel.src)
+    console.log({urlDir})
+    const svgPanel= renderComic(panel.alt, urlDir)
     panel.replaceWith(svgPanel)
   });
   }
