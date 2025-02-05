095f94df7ac2f5c12852120dd6ea41cf52c3f10a
Boris Marinov <marinovboris@protonmail.com>
2025-02-05 18:23:15 +0200

    stuff

--- editor.js
+++ editor.js
@@ -1,7 +1,9 @@
 import {renderComic} from './lib/index.js'
 
-document.getElementById('script').addEventListener('input', (e) => {
+const update = () => {
   const comic = document.getElementById('comic')
-  const script = e.target.value.split('* * *')
-  comic.replaceChildren(...script.map((panel) => renderComic(panel, "generic-comics/")))
-})
+  const script = document.getElementById('script').value.split('* * *')
+  comic.replaceChildren(...script.map((panel) => renderComic(panel, "comic")))
+}
+document.getElementById('script').addEventListener('input', update)
+update()
--- plugin.js
+++ plugin.js
@@ -4,7 +4,7 @@ const panels =
 Array.prototype.slice.call(document.getElementsByClassName("panel"))
 panels.forEach((panel) => {
   const urlDir = panel.src.split('/').slice(0, -1).join('/')
-  if (panel.height < 40) {
+  if (panel.height < 100) { // When the image isn't loaded
     const svgPanel= renderComic(panel.alt, urlDir)
     panel.replaceWith(svgPanel)
   } else {
