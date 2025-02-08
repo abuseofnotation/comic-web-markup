c07b3c2cf17c854d811c4742a5c60d227b63256c
Boris Marinov <marinovboris@protonmail.com>
2025-02-08 21:11:29 +0200

    add textSize

--- plugin.js
+++ plugin.js
@@ -4,21 +4,22 @@ const panels =
 Array.prototype.slice.call(document.getElementsByClassName("comic-web-panel"))
 panels.forEach((panel) => {
 
-  const config = document.getElementById("comic-web").dataset
+  const el = document.getElementById("comic-web")
+  const config = el ? el.dataset : {}
   //The if no URL for resources is specified, use the image src 
   const imageConfig = panel.dataset
   const urlDir =  imageConfig.urlDir || config.urlDir || panel.src.split('/').slice(0, -1).join('/')
-  const fileType = imageConfig.fileType || config.fileType || panel.src.split('.')[1]
+  const fileType = imageConfig.fileType || config.fileType || panel.src.split('.').at(-1)
   const width = imageConfig.width || config.width || 500
   const height = imageConfig.height || config.height || 500
+  const textSize = imageConfig.textSize || config.textSize || 15
 
-  const comic = () => renderComic(panel.alt, {urlDir, fileType, width, height})
+  const comic = () => renderComic(panel.alt, {urlDir, fileType, width, height, textSize})
 
   if (panel.height < 100) { // When the image isn't loaded
     panel.replaceWith(comic())
   } else {
   panel.addEventListener('error', () => {
-    const svgPanel= renderComic(panel.alt, {urlDir, fileType})
     panel.replaceWith(comic())
   });
   }
