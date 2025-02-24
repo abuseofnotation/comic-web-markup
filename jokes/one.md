9c890578923e3d29d43eea3bc6e8a8224b7d7145
Boris Marinov <marinovboris@protonmail.com>
2025-02-24 11:35:21 +0200

    Add the option to generate images

---
+++ jokes/one.md
@@ -0,0 +1,5 @@
+THE STUDIO
+JENCEL 
+So you are saying I have to *work* to succeed?
+Sorry, that's a deal-breaker for me!
+
--- plugin.js
+++ plugin.js
@@ -1,26 +1,28 @@
 import {renderComic} from './lib/index.js'
 
-const panels = 
-Array.prototype.slice.call(document.getElementsByClassName("comic-web-panel"))
-panels.forEach((panel) => {
-
+const getConfig = (imageConfig) => {
   const el = document.getElementById("comic-web")
   const config = el ? el.dataset : {}
   //The if no URL for resources is specified, use the image src 
-  const imageConfig = panel.dataset
   const urlDir =  imageConfig.urlDir || config.urlDir || panel.src.split('/').slice(0, -1).join('/')
   const fileType = imageConfig.fileType || config.fileType || panel.src.split('.').at(-1)
   const width = imageConfig.width || config.width || 500
   const height = imageConfig.height || config.height || 500
   const textSize = imageConfig.textSize || config.textSize || 15
+  return {urlDir, fileType, width, height, textSize}
+}
 
-  const comic = () => renderComic(panel.alt, {urlDir, fileType, width, height, textSize})
+window.comicWebRender = (text) => renderComic(text, getConfig({}))
+
+const panels = 
+Array.prototype.slice.call(document.getElementsByClassName("comic-web-panel"))
+panels.forEach((panel) => {
 
   if (panel.height < 100) { // When the image isn't loaded
-    panel.replaceWith(comic())
+    panel.replaceWith(renderComic(panel.alt, getConfig(panel.dataset)))
   } else {
   panel.addEventListener('error', () => {
-    panel.replaceWith(comic())
+    panel.replaceWith(renderComic(panel.alt, getConfig(panel.dataset)))
   });
   }
 })
